9c897d56cb0c4d581b65e2169688d601b9b63918
vrana132 <vrana132@gmail.com>
2020-05-31 11:44:09 +0530

    new changes

---
+++ PackageJarChallenge/src/academy/learnprogramming/Main.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming;
+
+import com.example.packages.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Series.factorial(5));
+        System.out.println(Series.nSum(4));
+        Series.fibonacci(6);
+    }
+}
